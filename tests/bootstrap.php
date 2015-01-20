9dae02c52b627a118b0fa2c84f79987abcd1104f
Moshe Brevda <mbrevda@gmail.com>
2015-01-20 07:59:41 +0200

    Remove unused bootstrap file

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL);
-
-$rootDir = dirname(__DIR__);
-
-require($rootDir . "/vendor/autoload.php");
-
-// need the fake response sender from the Aura.Web tests
-require($rootDir . "/vendor/aura/web/tests/unit/src/FakeResponseSender.php");
