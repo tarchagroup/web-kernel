5e5b09492175d5eb353421ec15db359cdb5d263e
shosh <shoshana@tarcha.com>
2015-03-12 14:22:51 +0000

    uncomment not working file

--- tests/src/Entities/AbstractEntityTest.php
+++ tests/src/Entities/AbstractEntityTest.php
@@ -1,68 +1,68 @@
-// <?php
+<?php
 
-// namespace Tarcha\WebKernel\Tests\Entities;
+namespace Tarcha\WebKernel\Tests\Entities;
 
-// use Tarcha\WebKernel\Tests\Test;
-// use Slug\Slugifier;
+use Tarcha\WebKernel\Tests\Test;
+use Slug\Slugifier;
 
-// class AbstractEntityTest extends Test
-// {
-//     public function getPayload($args)
-//     {
-//         $slugifier = new Slugifier();
-//         return new MockAbstractEntity($args, $slugifier);
-//     }
+class AbstractEntityTest extends Test
+{
+    public function getPayload($args)
+    {
+        $slugifier = new Slugifier();
+        return new MockAbstractEntity($args, $slugifier);
+    }
 
-//     public function testShouldNotBeDirtyOnInstanciation()
-//     {
-//         $e = $this->getPayload(['data' => 'foo']);
+    public function testShouldNotBeDirtyOnInstanciation()
+    {
+        $e = $this->getPayload(['data' => 'foo']);
 
-//         $this->assertEquals($e->data, 'foo');
-//         $this->assertFalse($e->isDirty());
-//     }
+        $this->assertEquals($e->data, 'foo');
+        $this->assertFalse($e->isDirty());
+    }
 
-//     public function testSetData()
-//     {
-//         $e = $this->getPayload(['data' => 'foo']);
+    public function testSetData()
+    {
+        $e = $this->getPayload(['data' => 'foo']);
 
-//         $e->setData(['data' => 'bar']);
-//         $this->assertEquals($e->data, 'bar');
-//     }
+        $e->setData(['data' => 'bar']);
+        $this->assertEquals($e->data, 'bar');
+    }
 
-//     public function testIsDirtyOnNewData()
-//     {
-//         $e = $this->getPayload(['data' => 'foo']);
+    public function testIsDirtyOnNewData()
+    {
+        $e = $this->getPayload(['data' => 'foo']);
 
-//         $e->setData(['data' => 'bar']);
-//         $this->assertTrue($e->isDirty());
-//     }
+        $e->setData(['data' => 'bar']);
+        $this->assertTrue($e->isDirty());
+    }
 
-//     public function testIsNotDirtyOnNoNewData()
-//     {
-//         $e = $this->getPayload(['data' => 'foo']);
+    public function testIsNotDirtyOnNoNewData()
+    {
+        $e = $this->getPayload(['data' => 'foo']);
 
-//         $e->setData(['data' => 'foo']);
-//         $this->assertFalse($e->isDirty());
-//     }
+        $e->setData(['data' => 'foo']);
+        $this->assertFalse($e->isDirty());
+    }
 
-//     public function testGetData()
-//     {
-//         $e = $this->getPayload(['data' => 'foo']);
+    public function testGetData()
+    {
+        $e = $this->getPayload(['data' => 'foo']);
 
-//         $this->assertEquals($e->getData(), ['data' => 'foo', 'slug' => null]);
-//     }
+        $this->assertEquals($e->getData(), ['data' => 'foo', 'slug' => null]);
+    }
 
-//     public function testSetOnlyPredefinedVariables()
-//     {
-//         $e = $this->getPayload(['data' => 'foo', 'bar' => 'baz']);
+    public function testSetOnlyPredefinedVariables()
+    {
+        $e = $this->getPayload(['data' => 'foo', 'bar' => 'baz']);
 
-//         $this->assertEquals($e->getData(), ['data' => 'foo', 'slug' => null]);
-//     }
+        $this->assertEquals($e->getData(), ['data' => 'foo', 'slug' => null]);
+    }
 
-//     public function testJsonSerialize()
-//     {
-//         $e = $this->getPayload(['data' => 'foo']);
+    public function testJsonSerialize()
+    {
+        $e = $this->getPayload(['data' => 'foo']);
 
-//         $this->assertEquals(json_encode($e), json_encode(['data' => 'foo', 'slug' => null]));
-//     }
-// }
+        $this->assertEquals(json_encode($e), json_encode(['data' => 'foo', 'slug' => null]));
+    }
+}
